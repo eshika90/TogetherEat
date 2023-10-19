1e16c51d3e1b62fae38bc6e980cbe0bae57fd25b
Kim Seryung <129057141+eshika90@users.noreply.github.com>
2023-10-19 21:59:45 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
 PROJECT_ROOT="/home/ubuntu/app"
 APP_NAME="project"
-
 TIME_NOW=$(date +%c)
-
 cd $PROJECT_ROOT
 
 pm2 delete $APP_NAME
-pm2 start npm --name $APP_NAME -- start
+pm2 start main.js
 
 echo "$TIME_NOW > Deploy has been completed"
