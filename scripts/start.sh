749dcaccf3ac256ab1e25d9670dd0a49bc9902b7
Kim Seryung <129057141+eshika90@users.noreply.github.com>
2023-10-19 09:29:09 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,11 @@ TIME_NOW=$(date +%c)
 
 cd $PROJECT_ROOT
 
-npm start:dev
+# 현재 실행 중인 모든 pm2 프로세스 삭제
+pm2 delete all
+
+# 빌드 및 실행
+npm run build
+pm2 start dist/main.js --name "$APP_NAME"
 
 echo "$TIME_NOW > Deploy has been completed"
