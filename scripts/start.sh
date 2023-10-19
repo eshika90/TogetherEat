7b6fe0d246a81ce901a6009dfbefd35d629d861d
Kim Seryung <129057141+eshika90@users.noreply.github.com>
2023-10-19 21:53:57 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
+PROJECT_ROOT="/app"
 APP_NAME="project"
 
 TIME_NOW=$(date +%c)
-
 cd $PROJECT_ROOT
-
-# 기존에 실행 중인 pm2 프로세스 중단
 pm2 delete $APP_NAME
-
-# pm2로 npm start:dev 실행
-pm2 start npm --name $APP_NAME -- run start:dev
-
+pm2 start npm --name $APP_NAME -- start
 echo "$TIME_NOW > Deploy has been completed"
